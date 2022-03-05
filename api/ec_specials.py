b9787fa43c1bc2cbebeaf4570fc52840d7ea4efa
AndiBraimllari <andibraimllari@gmail.com>
2022-03-05 16:30:15 +0100

    account for omitted ë and ç

---
+++ api/ec_specials.py
@@ -0,0 +1,18 @@
+# TODO improve algorithm
+def generate_ec_permutations(word):
+    perms = ['']
+
+    if word is None:
+        return perms
+
+    specials_map = {'e': ['e', 'ë'], 'c': ['c', 'ç']}
+    for char in word:
+        if char != 'e' and char != 'c':
+            perms = [perm + char for perm in perms]
+        else:
+            some_perms = [perm + specials_map[char][0] for perm in perms]
+            other_perms = [perm + specials_map[char][1] for perm in perms]
+            perms = some_perms + other_perms
+
+    perms.remove(word)
+    return perms

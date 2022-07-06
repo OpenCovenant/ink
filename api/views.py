14de3c462a403f2ba21da4365a14f232712c16b1
AndersonCeci <37506005+AndersonCeci@users.noreply.github.com>
2022-07-06 22:21:28 +0200

    adding Word Feature (#12)
    * adding Word Feature
    
    * made the required changes

--- api/views.py
+++ api/views.py
@@ -2,9 +2,9 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
 import PyPDF2
+import docx
 
 from marking.marking_generator import generate_markings
-
 
 @api_view(['GET'])
 def ping(request):
@@ -46,14 +46,22 @@ def translation(request):
 
     return JsonResponse(textInEnglish)
 
-
 @api_view(["POST"])
 def upload_document(request):
+    print(len(request.FILES))
+    
     if len(request.FILES) != 1:
+        
         raise ValueError('')
 
     uploaded_file_key = list(request.FILES.keys())[0]
     file = request.FILES[uploaded_file_key]
+    
+    microsoftFormat = docx.Document(file)
+    
+    fullText = []
+    for para in microsoftFormat.paragraphs:
+        fullText.append(para.text)
 
     text = ''
     if file.content_type == 'application/pdf':
@@ -62,10 +70,11 @@ def upload_document(request):
     elif file.content_type == 'application/vnd.oasis.opendocument.text':
         pass  # .odt
     elif file.content_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-        pass  # .docx
+        for page in fullText:
+            text += page
     elif file.content_type == 'application/msword':
-        pass  # .doc
-    else:
+        pass# .doc   
+    else:   
         raise ValueError('Document format not supported!')
 
     content = generate_markings(text)
